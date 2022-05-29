81a8289e23a72a5075179a082f3b2df547382ea3
Yashchenko Victor <Andrey@Komp11.local>
2022-05-29 20:00:55 +0300

    fixing2'

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -1,11 +1,11 @@
 import GameSavingLoader from '../js/GameSavingLoader';
 
-test('testing GameSavingLoader result', async () => {
+test('testing GameSavingLoader result', () => {
   GameSavingLoader.load().then((saving) => {
-  	expect(JSON.stringify(saving)).toEqual(JSON.stringify({
+  	expect(saving).toEqual({
 	  id: 9,
 	  created: 1546300800,
 	  userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }
-	}));
+	});
   });
 });
